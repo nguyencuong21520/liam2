1490615bad9def7251f1a0ef04dee9e9de21d42f
nguyencuong21520 <nguyencuong21520@gmail.com>
2025-03-15 01:39:00 +0700

    build 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,20 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    cssMinify: true,
     cssCodeSplit: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          if (!assetInfo.name) return "assets/[name]-[hash][extname]";
+          const extType = assetInfo.name.split(".").at(1) || "";
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/img/[name]-[hash][extname]`;
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
       },
     },
   },
